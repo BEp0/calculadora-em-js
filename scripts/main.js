3b65f3cfe7313ad892033c56dff02d56dde17ddb
Bernardo Porto e Silva Souza <72446326+BEp0@users.noreply.github.com>
2021-02-20 09:32:44 -0300

    rework

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,3 @@
-var resposta = document.getElementById("resposta")
-var n1 = document.getElementById("number1")
-var n2 = document.getElementById("number2")
-var n3 = document.getElementById("number3")
-var n4 = document.getElementById("number4")
-var n5 = document.getElementById("number5")
-var n6 = document.getElementById("number6")
-var n7 = document.getElementById("number7")
-var n8 = document.getElementById("number8")
-var n9 = document.getElementById("number9")
-function number(num) {
-    var numero = "";
-    resposta.innerHTML = Number(num.value)// n concatena
+function botao(){
+    document.calc.visor.value = "7";
 }
